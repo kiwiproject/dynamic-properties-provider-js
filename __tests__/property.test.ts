15d9c7848430c6fcab90242662cc3569d7f148b2
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2024-06-07 10:35:33 -0400

    Sonar fixes
    Also ran the formatter

--- __tests__/property.test.ts
+++ __tests__/property.test.ts
@@ -1,9 +1,19 @@
-import {describe, expect, it} from "@jest/globals";
-import {Property} from "../src/property";
+import { describe, expect, it } from "@jest/globals";
+import { Property } from "../src/property";
 
 describe("Property", () => {
   it("should allow setup from builder", () => {
-    const prop = Property.newProperty().setName("name").setType("string").setLabel("Property Name").setVisible(true).setEditable(true).setSensitive(false).setRequired(false).setUnits(["m", "km"]).setDefaultUnit("km").setValues(["blah"]);
+    const prop = Property.newProperty()
+      .setName("name")
+      .setType("string")
+      .setLabel("Property Name")
+      .setVisible(true)
+      .setEditable(true)
+      .setSensitive(false)
+      .setRequired(false)
+      .setUnits(["m", "km"])
+      .setDefaultUnit("km")
+      .setValues(["blah"]);
 
     expect(prop.name).toEqual("name");
     expect(prop.type).toEqual("string");
--- src/property-extractor.ts
+++ src/property-extractor.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {Property} from "./property";
+import { Property } from "./property";
 
 /**
  * Attempts to pull the list of dynamic properties for a given object.
@@ -9,7 +9,9 @@ import {Property} from "./property";
  */
 function getPropertyListForObject(obj: any): Array<Property> {
   if (!obj.getDynamicProperties) {
-    throw new Error('The given object does not have a "getDynamicProperties" method. Can not process property list.');
+    throw new Error(
+      'The given object does not have a "getDynamicProperties" method. Can not process property list.',
+    );
   }
 
   return obj.getDynamicProperties();
@@ -19,11 +21,13 @@ function getPropertyListForObject(obj: any): Array<Property> {
  * Sets up endpoints to add to an express server to return dynamic properties
  * @param objects The map of identifiers to an object. This object will ultimately be passed to #getPropertyListForObject
  */
-function setupDynamicPropertiesEndpoints(objects: Record<string, any>): express.Router {
+function setupDynamicPropertiesEndpoints(
+  objects: Record<string, any>,
+): express.Router {
   const router = express.Router();
 
   router.get("/kiwi/dynamic-properties", (_req, res) => {
-    const props:{} = {};
+    const props: object = {};
     for (const key of Object.keys(objects)) {
       props[key] = getPropertyListForObject(objects[key]);
     }
@@ -32,14 +36,16 @@ function setupDynamicPropertiesEndpoints(objects: Record<string, any>): express.
   });
 
   router.get("/kiwi/dynamic-properties/:identifier", (req, res) => {
-    const objectProps = getPropertyListForObject(objects[req.params.identifier]);
+    const objectProps = getPropertyListForObject(
+      objects[req.params.identifier],
+    );
     res.json(objectProps);
-  })
+  });
 
   return router;
 }
 
 export const PropertyExtractor = {
   getPropertyListForObject,
-  setupDynamicPropertiesEndpoints
+  setupDynamicPropertiesEndpoints,
 };
